8533989b312a064d841e58756987a6e8f076d8d2
andressasmarques <andressamarques.biotec@gmail.com>
2024-05-22 12:54:18 -0300

    Atualização do projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Teste com iPhone:");
+        Smartphone iphone = new Iphone("123456", "iPhone 13", "111111111111111", 128);
+        iphone.Ligar();
+        iphone.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\nTeste com Nokia:");
+        Smartphone nokia = new Nokia("654321", "Nokia 3310", "222222222222222", 32);
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Telegram");
+    }
+}
